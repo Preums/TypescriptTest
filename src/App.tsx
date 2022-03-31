6e2721711d2da28f2662399e21e2dd59a6f205ae
Preums <preums@gmx.com>
2022-03-31 11:41:28 +0200

    react typescript test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import { FC } from 'react';
+
+import Header from './components/Header';
+import Todos from './components/todos/Todos';
+import Counter from './components/counter/Counter';
+
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App:FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <Header name='typescript world'/>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <Counter />
+      <Todos />
+    </header>
+  </div>
+);
 
 export default App;
